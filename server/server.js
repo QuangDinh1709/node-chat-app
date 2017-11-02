602edee8d70ac03ecfacc0390aab74b8f19ab53f
Quang Dinh <quangdinh1709@gmail.com>
2017-11-02 16:41:53 +0700

    Greet new user, and alert others

--- server/server.js
+++ server/server.js
@@ -28,13 +28,29 @@ io.on('connection', (socket) => {
   //   createAt : 123123
   // });
 
+  socket.emit('newMessage', {
+    from : 'Admin',
+    text : 'Welcome to the chat app'
+  });
+
+  socket.broadcast.emit('newMessage', {
+    from : 'Admin',
+    text : 'New user jonied',
+    createAt : new Date().getTime()
+  });
+
   socket.on('createMessage', (message)=>{
       console.log('createMessage', message);
       io.emit('newMessage',{
         from : message.from,
         text : message.text,
         createAt : new Date().getTime()
-      })
+      });
+      // socket.broadcast.emit('newMessage',{
+      //   form : message.from,
+      //   text : message.text,
+      //   createAt : new Date().getTime()
+      // });
   });
 
   socket.on('createEmail', (newEmail) =>{
